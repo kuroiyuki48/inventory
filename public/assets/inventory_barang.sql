ba6b3525f5717713fd6580470b290b612edf633e
Noor Alizah <4340264-nooralizahar@users.noreply.gitlab.com>
2021-06-11 13:38:05 +0700

    update database

--- public/assets/inventory_barang.sql
+++ public/assets/inventory_barang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 10, 2021 at 06:20 PM
+-- Generation Time: Jun 11, 2021 at 06:37 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.4.0
 
@@ -32,6 +32,8 @@ DROP TABLE IF EXISTS `barang`;
 CREATE TABLE IF NOT EXISTS `barang` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama_barang` varchar(255) DEFAULT NULL,
+  `merk` varchar(255) DEFAULT NULL,
+  `jenis` varchar(255) DEFAULT NULL,
   `harga` int(11) DEFAULT 0,
   `stok` int(11) NOT NULL DEFAULT 0,
   `created_at` datetime DEFAULT NULL,
@@ -43,10 +45,10 @@ CREATE TABLE IF NOT EXISTS `barang` (
 -- Dumping data for table `barang`
 --
 
-INSERT INTO `barang` (`id`, `nama_barang`, `harga`, `stok`, `created_at`, `updated_at`) VALUES
-(1, 'Sapu', 10000, 8, '2021-06-10 00:00:00', '2021-06-10 17:49:39'),
-(2, 'Baskom', 5000, 23, '2021-06-10 00:00:00', '2021-06-10 18:19:02'),
-(3, 'Pengki', 7000, 25, '2021-06-10 15:52:57', '2021-06-10 17:46:56');
+INSERT INTO `barang` (`id`, `nama_barang`, `merk`, `jenis`, `harga`, `stok`, `created_at`, `updated_at`) VALUES
+(1, 'Sapu', NULL, NULL, 10000, 8, '2021-06-10 00:00:00', '2021-06-10 17:49:39'),
+(2, 'Baskom', NULL, NULL, 5000, 20, '2021-06-10 00:00:00', '2021-06-11 02:59:29'),
+(3, 'Pengki', 'Yamaha', 'Alat Rumah Tangga', 7000, 21, '2021-06-10 15:52:57', '2021-06-11 02:59:23');
 
 -- --------------------------------------------------------
 
@@ -63,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `barang_keluar` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `barang_keluar`
@@ -73,7 +75,8 @@ INSERT INTO `barang_keluar` (`id`, `id_pelanggan`, `total_harga`, `tanggal`, `cr
 (1, 1, 50000, '2021-06-10', '2021-06-10 00:00:00', '2021-06-10 00:00:00'),
 (2, 1, 25000, '2021-06-10', '2021-06-10 00:00:00', '2021-06-10 17:32:54'),
 (3, 2, 30000, '2021-06-10', '2021-06-10 16:41:04', '2021-06-10 17:16:11'),
-(6, 3, 35000, '2021-06-12', '2021-06-10 17:46:43', '2021-06-10 17:46:56');
+(6, 3, 35000, '2021-06-12', '2021-06-10 17:46:43', '2021-06-10 17:46:56'),
+(7, 3, 43000, '2021-06-12', '2021-06-11 02:59:10', '2021-06-11 02:59:29');
 
 -- --------------------------------------------------------
 
@@ -91,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `barang_keluar_detail` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `barang_keluar_detail`
@@ -102,7 +105,9 @@ INSERT INTO `barang_keluar_detail` (`id`, `id_barang_keluar`, `id_barang`, `juml
 (2, 2, 2, 5, 25000, '2021-06-10 00:00:00', '2021-06-10 00:00:00'),
 (5, 3, 2, 2, 10000, '2021-06-10 17:13:42', '2021-06-10 17:13:42'),
 (6, 3, 1, 2, 20000, '2021-06-10 17:16:11', '2021-06-10 17:16:11'),
-(14, 6, 3, 5, 35000, '2021-06-10 17:46:56', '2021-06-10 17:46:56');
+(14, 6, 3, 5, 35000, '2021-06-10 17:46:56', '2021-06-10 17:46:56'),
+(16, 7, 2, 3, 15000, '2021-06-11 02:59:29', '2021-06-11 02:59:29'),
+(15, 7, 3, 4, 28000, '2021-06-11 02:59:23', '2021-06-11 02:59:23');
 
 -- --------------------------------------------------------
 
@@ -277,6 +282,7 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` int(11) DEFAULT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -285,14 +291,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`) USING HASH
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Endang', 'endang@gmail.com', NULL, '$2y$10$pL4RKxhn4X1QyhIQlajDUeJPhvTZIG5OJ/3jfOmsdXZorNFdjIpr6', NULL, '2021-06-10 04:49:14', '2021-06-10 04:49:14');
+INSERT INTO `users` (`id`, `name`, `role`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Endang', 1, 'endang@gmail.com', NULL, '$2y$10$pL4RKxhn4X1QyhIQlajDUeJPhvTZIG5OJ/3jfOmsdXZorNFdjIpr6', NULL, '2021-06-10 04:49:14', '2021-06-10 04:49:14'),
+(2, 'Supervisor', NULL, 'supervisor@gmail.com', NULL, '$2y$10$tacCxTXwzGoncaKPKBZvtOE0buyftJ8aiFXVS0X6as/mv/B.PfLyK', NULL, '2021-06-10 20:01:47', '2021-06-10 20:01:47');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
